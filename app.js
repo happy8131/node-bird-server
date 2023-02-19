6d3c32344f55f618137a6a5e1f56969e474bc684
happy8131 <lion8131@naver.com>
2023-02-19 19:28:34 +0900

    cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const postRouter = require("./routes/post");
+const userRouter = require("./routes/user");
 const db = require("./models");
 
 const app = express();
@@ -9,6 +11,14 @@ db.sequelize
     console.log("db 연결 성공!!");
   })
   .catch(console.error);
+
+app.use(
+  cors({
+    origin: "*",
+  })
+); // cors 설정해준다
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // 프론트에서 넘어온 데이터를 req.body넣어주는 역할을 해준다
 
 app.get("/", (req, res) => {
   res.send("hello express");
@@ -27,6 +37,7 @@ app.get("/api/posts", (req, res) => {
 });
 
 app.use("/post", postRouter);
+app.use("/user", userRouter);
 
 app.listen(3065, () => {
   console.log("실행");
