024e647ac8e6a83089820d87cbb79de918487285
happy8131 <lion8131@naver.com>
2023-03-12 00:00:58 +0900

    retweet/ querystring lastid

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 const exporess = require("express");
+const { Op } = require("sequelize");
 const { Post, Image, User, Comment } = require("../models");
 
 const router = exporess.Router();
@@ -7,7 +8,13 @@ router.get("/", async (req, res, next) => {
   //GET/ posts
 
   try {
+    const where = {};
+    if (parseInt(req.query.lastId, 10)) {
+      // 초기 로딩이 아닐 때
+      where.id = { [Op.lt]: parseInt(req.query.lastId, 10) };
+    } // 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
     const posts = await Post.findAll({
+      where,
       limit: 10,
       order: [
         ["createdAt", "DESC"],
@@ -26,6 +33,19 @@ router.get("/", async (req, res, next) => {
           model: User, // 좋아요 누른사람
           as: "Likers",
           attributes: ["id"],
+        },
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+            {
+              model: Image,
+            },
+          ],
         },
       ],
     }); // 모든 게시글을 가져온다
